0bd95a2b648f403110d4ae8b4aee18d472b05d77
pandi.v <pandi.v@LP2-AP-51628337.PROD.HCLPNP.COM>
2022-02-17 21:51:45 +0530

    Firsr commit

---
+++ Myapp/src/test/java/mypackage/FirstTest.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void method()
+	{
+		System.out.println("Here is my first test.");
+	}
+
+}
